36d0a873ab9a7349062b7c0f4b3faa75718c4171
Chris Pennington <chris@codinginpublic.dev>
2024-05-31 15:12:19 -0600

    login page

---
+++ src/middleware.ts
@@ -0,0 +1,41 @@
+import { defineMiddleware } from "astro:middleware";
+import { lucia } from "@/auth";
+
+export const onRequest = defineMiddleware(async (context, next) => {
+  const sessionId = context.cookies.get(lucia.sessionCookieName)?.value ?? null;
+
+  const { pathname } = context.url;
+
+  if (!sessionId) {
+    const sessionCookie = lucia.createBlankSessionCookie();
+    context.cookies.set(
+      sessionCookie.name,
+      sessionCookie.value,
+      sessionCookie.attributes
+    );
+    context.locals.user = null;
+    context.locals.session = null;
+
+    if (pathname !== "/login" && context.request.method === "GET") {
+      return context.redirect("/login");
+    }
+  }
+
+  if (sessionId) {
+    const { session, user } = await lucia.validateSession(sessionId);
+    if (session && session.fresh) {
+      const sessionCookie = lucia.createSessionCookie(session.id);
+      context.cookies.set(
+        sessionCookie.name,
+        sessionCookie.value,
+        sessionCookie.attributes
+      );
+    }
+    context.locals.session = session;
+    context.locals.user = user;
+    if (pathname === "/create" || pathname === "/login") {
+      return context.redirect("/");
+    }
+  }
+  return next();
+});
